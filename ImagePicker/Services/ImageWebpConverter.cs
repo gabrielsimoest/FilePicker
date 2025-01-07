df9305a2588af9f7af8fad40ff15eff6fa1cef77
Gabriel Simões <gabrielsimoest@gmail.com>
2025-01-07 11:35:57 -0300

    Feat: Compression Method

--- ImagePicker/Services/ImageWebpConverter.cs
+++ ImagePicker/Services/ImageWebpConverter.cs
@@ -1,7 +1,9 @@
-﻿using SixLabors.ImageSharp;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Webp;
-using System.IO;
-using System.Threading.Tasks;
+using SixLabors.ImageSharp.PixelFormats;
+using Image = SixLabors.ImageSharp.Image;
 
 namespace ImagePicker.Services
 {
@@ -10,21 +12,31 @@ namespace ImagePicker.Services
         public async Task<Entities.Image> ConvertToWebP(Entities.Image image)
         {
             using var inStream = new MemoryStream(image.File);
-            var myImage = await Image.LoadAsync(inStream);
-            using var outStream = new MemoryStream();
+            using var originalImage = System.Drawing.Image.FromStream(inStream);
 
-            var encoder = new WebpEncoder
+            using var compressedStream = new MemoryStream();
+
+            var encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, 170L);
+
+            var jpgEncoder = ImageCodecInfo.GetImageEncoders().FirstOrDefault(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
+            originalImage.Save(compressedStream, jpgEncoder, encoderParams);
+
+            compressedStream.Seek(0, SeekOrigin.Begin);
+
+            using var imageSharp = await Image.LoadAsync<Rgba32>(compressedStream);
+
+            using var webpStream = new MemoryStream();
+            var webpEncoder = new WebpEncoder()
             {
-                Quality = 100,
-                Method = WebpEncodingMethod.BestQuality,
-                FilterStrength = 0
+                Quality = 80
             };
 
-            await myImage.SaveAsync(outStream, encoder);
+            await imageSharp.SaveAsync(webpStream, webpEncoder);
 
             return new Entities.Image(image.Id)
             {
-                File = outStream.ToArray(),
+                File = webpStream.ToArray(),
                 Extension = "image/webp"
             };
         }
